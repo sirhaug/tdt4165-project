ab6d74c09870b29611e5a188070be8816a39ffca
Jørgen Sirhaug <sirhaug751@gmail.com>
2015-11-02 13:16:08 +0100

    Same as last commit, only this time the new stuff is actually IN the commit...

---
+++ part1/project/src/main/scala/Bank.scala
@@ -0,0 +1,19 @@
+
+
+object Bank {
+  
+  private var idCounter: Int = 0
+  
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+
+    from.withdraw(amount)
+    to.deposit(amount)
+
+  }
+  
+  def getUniqueId: Int = {
+    idCounter += 1
+    idCounter
+  }
+  
+}
